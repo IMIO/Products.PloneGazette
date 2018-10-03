ef1fcd1b26fa5836e42afc4e115494bdb9130107
Benoit Suttor <bsuttor@imio.be>
2018-10-03 16:51:37 +0200

    Preparing release 3.2.5.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (
 
 setup(
     name='Products.PloneGazette',
-    version='3.2.5.2.dev0',
+    version='3.2.5.2',
     author='Pilot Systems, Nidelven IT LTD and others',
     author_email='',
     maintainer='Morten W. Petersen',
